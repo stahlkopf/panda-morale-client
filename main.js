f2b3537a2d88bb1cef171abd1d899085b302e1fa
stahlkopf <btaylertech@gmail.com>
2017-11-27 23:42:06 -0500

    changing form

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+const os = require('os')
+
+const osName = os.platform()
+const osType = os.type()
+const osBit = os.arch()
+const pcName = os.hostname()
+const nInterface = Object.values(os.networkInterfaces())[1][0].address
+const userName = os.userInfo().username
+
 var menubar = require('menubar');
 
 var mb = menubar({
@@ -7,10 +16,13 @@ var mb = menubar({
 	height: 180
 });
 
+
+
 mb.on('ready', function ready(){
 	console.log('App is ready');
 });
 
 mb.on('after-create-window', function windowLoaded(){
 	console.log('App started');
+	console.log(pcName)
 });
