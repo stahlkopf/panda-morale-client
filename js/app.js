8b626b63b27386a33f3d5f62c093fcdbae0a1078
stahlkopf <btaylertech@gmail.com>
2018-01-06 23:16:48 -0500

    Added form changes

--- js/app.js
+++ js/app.js
@@ -2,37 +2,21 @@ angular.module('nodeTodo', [])
 .controller('mainController', ($scope, $http) => {
   $scope.formData = {};
   $scope.todoData = {};
-  $scope.formData.userid = require('os').hostname();
-  // Get all todos
-  $http.get('http://localhost:3000/api/v1/todos')
-  .success((data) => {
-    $scope.todoData = data;
-    console.log(data);
-  })
-  .error((error) => {
-    console.log('Error: ' + error);
-  });
+  $scope.formData.anonymous = false;
+
   // Create a new todo
   $scope.createTodo = () => {
+
+    $scope.formData.userid = require("os").userInfo().username;
     $http.post('http://localhost:3000/api/v1/todos', $scope.formData)
     .success((data) => {
       $scope.formData = {};
-      $scope.todoData = data;
+      $scope.formData.anonymous = false;s
       console.log(data);
     })
     .error((error) => {
       console.log('Error: ' + error);
     });
   };
-  // Delete a todo
-  $scope.deleteTodo = (todoID) => {
-    $http.delete('http://localhost:3000/api/v1/todos/' + todoID)
-    .success((data) => {
-      $scope.todoData = data;
-      console.log(data);
-    })
-    .error((data) => {
-      console.log('Error: ' + data);
-    });
-  };
+
 });
